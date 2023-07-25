513eb7c68a91c2cd936b240a608d39afcc011ca5
MaiconConstancio <135743035+MaiconConstancio@users.noreply.github.com>
2023-07-25 17:18:20 -0300

    Add files via upload

---
+++ Aula12Atividade/Program.cs
@@ -0,0 +1,49 @@
+﻿using Aula12Atividade;
+
+Console.WriteLine("ShoppiMK\n\n");
+
+produtos produto01 = new produtos();
+
+produto01.nome = "Suco de Laranja";
+produto01.descricao = "Sucos extraidos das laranjas mais puras e saborosas";
+produto01.quantidade = 250;
+produto01.preco = 7.50;
+produto01.status = true;
+
+produto01.mostra();
+
+produtos produto02 = new produtos();
+
+produto02.nome = "Suco de Morango";
+produto02.descricao = "Morangos meticulosamente selecionados";
+produto02.quantidade = 450;
+produto02.preco = 8.50;
+produto02.status = true;
+
+produto02.mostra();
+
+produtos produto03 = new produtos();
+
+produto03.nome = "Suco de Maracuja";
+produto03.descricao = "Maracujas ricos em proteinas e os melhores";
+produto03.quantidade = 350;
+produto03.preco = 7.50;
+produto03.status = true;
+
+produto03.mostra();
+
+
+
+produto01.desativa();
+produto02.desativa();
+
+produto01.mostra();
+produto02.mostra();
+produto03.mostra();
+
+
+
+
+Console.ReadKey();
+
+
---
+++ Aula12Atividade/produtos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Atividade
+{
+    internal class produtos
+    {
+        public string nome;
+        public string descricao;
+        public double quantidade;
+        public double preco;
+        public bool status;
+        
+        public void mostra()
+        {
+            Console.WriteLine("Confirmação de seu produto:");
+            Console.WriteLine($"produto escolhido: {nome}, ");
+            Console.WriteLine($"descrição deste produto: {descricao}");
+            Console.WriteLine($"Temos: {quantidade} quantidades.");
+            Console.WriteLine($"no valor de {preco}");
+            Console.WriteLine($"{status}\n\n");
+        }
+        public void desativa()
+        {
+            quantidade = 0;
+            status = false;
+        }
+
+
+
+    }
+}
